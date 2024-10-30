0394da22ded19bffaa14ec236759fe359ff2398d
Eugene <giveuper39@gmail.com>
2024-10-30 22:46:09 +0300

    added example tests, fixed some strange db interactions

--- client/db_methods.py
+++ client/db_methods.py
@@ -1,24 +1,27 @@
-from db import Data, db
+from db import Data
+from peewee import SqliteDatabase
 
 
-def init_db() -> None:
+def init_db(db: SqliteDatabase) -> None:
     """Connect to the database and create the tables.
 
+    :param db: SqliteDatabase instance
     :return:
     """
+    db.bind([Data], bind_refs=False, bind_backrefs=False)
     db.connect()
-    db.create_tables([Data], safe=True)
+    db.create_tables([Data])
     db.close()
 
 
-def insert_price(ticker: str, price: float, timestamp: int) -> None:
+def insert_price(ticker: str, price: float, timestamp: int, db: SqliteDatabase) -> None:
     """Insert a new price into the database.
 
     :param ticker: Currency ticker (e.g. 'btc', 'eth')
     :param price: Currency price
     :param timestamp: Current UNIX timestamp
+    :param db: SqliteDatabase instance
     :return:
     """
-    db.connect()
-    Data.create(ticker=ticker, price=price, timestamp=timestamp)
-    db.close()
+    with db.atomic():
+        Data.create(ticker=ticker, price=price, timestamp=timestamp)
